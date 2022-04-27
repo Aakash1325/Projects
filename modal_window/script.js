90b20c7b7cc92cedd43cd3b323ca65ff27596254
Aakash <aakash7065227814@gmail.com>
2022-04-27 14:41:46 +0530

    Add modal window open button

---
+++ modal_window/script.js
@@ -0,0 +1,31 @@
+'use strict';
+
+
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsShowModal = document.querySelectorAll('.show-modal');
+
+
+console.log(btnsShowModal);
+
+
+for(let i = 0 ; i < btnsShowModal.length; i++) {
+  btnsShowModal[i].addEventListener('click', function(){
+    //  btnsShowModal[i].textContent  = 'you clicked the button'
+     modal.classList.remove('hidden');
+     overlay.classList.remove('hidden');
+  });
+}
+
+btnCloseModal.addEventListener('click', function() {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
+
+overlay.addEventListener('click', function() {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
+
