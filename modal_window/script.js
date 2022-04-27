2ff81ada31c41c66d82e5da0278865e7402c013e
Aakash <aakash7065227814@gmail.com>
2022-04-27 20:12:07 +0530

    add Escape event

--- modal_window/script.js
+++ modal_window/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 
-
+/*
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
@@ -28,4 +28,47 @@ overlay.addEventListener('click', function() {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 });
+
+*/
+
+
+const modal = document.querySelector('.modal');
+const showModal = document.querySelectorAll('.show-modal');
+const overlay = document.querySelector('.overlay');
+const closeModal = document.querySelector('.close-modal');
+
+
+// clock button function 
+const openModalBtn =  function() {
+  console.log('remove hidden class')
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+
+const closeModalBtn = function() {
+  console.log("add hidden class ")
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+
+for(let i = 0; i < showModal.length; i++) {
+  showModal[i].addEventListener('click', openModalBtn)
+}
+
+closeModal.addEventListener('click', closeModalBtn);
+overlay.addEventListener('click', closeModalBtn);
+
+
+document.addEventListener('keydown', function(e) {
+  // console.log("key was pressed!");
+  // console.log(e.key);
+  if(e.key === 'Escape' && ! modal.classList.contains('hidden')) {
+    //  if(! modal.classList.contains('hidden')) {
+      closeModalBtn();
+    //  }
+  }
+});
+ 
+
 
